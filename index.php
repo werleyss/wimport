eb547a8642af55bd4a7fc1396046592ef27c9865
Werley <werley_desouzasilva@hotmail.com>
2018-04-05 01:14:01 -0300

    Primeiro projeto para github

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>wimport</title>
+    </head>
+    <body>
+        <h1>Importe para banco de dados</h1> 
+        <form method="POST" enctype="multipart/form-data" action="processa.php">
+            <label>Arquivo</label>
+            <input type="file" name="arquivo">
+            <input type="submit" value="Importar">
+        </form>
+    </body>
+</html>
+
---
+++ processa.php
@@ -0,0 +1,30 @@
+<?php
+
+//$arquivo = $_FILES['arquivo'];
+//var_dump($arquivo);
+$arquivo_tmp = $_FILES['arquivo']['tmp_name'];
+//var_dump($arquivo_tmp);
+
+$dados = file($arquivo_tmp);
+var_dump($dados);
+
+//$i = 0;
+
+/*foreach($dados as $linha){
+    
+    $linha = trim($linha);
+    $valor = explode(',', $linha);
+    //var_dump($valor);
+    if(!empty($valor[$i])){
+        echo "Nome: ".$valor[$i]."<br>";
+        $i = $i + 1;
+        echo "Profissao: ".$valor[$i]."<br>";
+        $i = $i + 1;
+        echo "Experiencia: ".$valor[$i]."<br><br>";
+        $i = 0;
+    }
+    
+    
+}*/
+
+?>
